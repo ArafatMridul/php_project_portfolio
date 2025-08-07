eff7c609b10542afdabc1a083463506a203b0c30
Arafat Mridul <mridularafat735@gmail.com>
2025-08-08 03:36:09 +0600

    completed till about section

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <!-- <link rel="icon" type="image/svg+xml" href="/public/favicon.svg" /> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>| Arafat Mridul |</title>
+    <link rel="stylesheet" href="styles/global.css">
+</head>
+
+<body>
+
+    <div class="grid_container">
+        <div class="grid_item intro">
+            <img class="avatar" src="assets/avatar.svg" alt="avatar">
+            <h1>Hello, I'm Mridul. <span>I like to build creative websites.</span></h1>
+        </div>
+
+        <div class="grid_item flex-center github">
+            <a href="https://github.com/ArafatMridul" target="_blank"><img src=" assets/github.svg" alt=""></a>
+        </div>
+
+        <div class="grid_item flex-center linkedin">
+            <a href="https://www.linkedin.com/in/arafat-mridul-460350243/" target="_blank"><img
+                    src="assets/linkedin.svg" alt=""></a>
+        </div>
+
+        <div class="grid_item flex-center facebook">
+            <a href="https://www.facebook.com/arafat.mridul" target="_blank"><img src="assets/facebook.svg" alt=""></a>
+        </div>
+
+        <div class="grid_item flex-center fm">
+            <a href="https://www.frontendmentor.io/profile/ArafatMridul" target="_blank"><img src="assets/code.svg"
+                    alt=""></a>
+        </div>
+
+        <div class="grid_item about">
+            <h2>A passionate <span>Computer Science</span> student with a deep interest in <span>Web
+                    development.</span> I enjoy building <span>Clean</span>, <span>Functional</span>, and
+                <span>Responsive</span>
+                websites.
+            </h2>
+        </div>
+        <div class="grid_item skills">
+            <h2>Skills</h2>
+        </div>
+        <div class="grid_item projects">
+            <h2>Projects</h2>
+        </div>
+        <div class="grid_item contact">
+            <h2>Contact Me!</h2>
+        </div>
+    </div>
+</body>
+
+</html>
